c27d489f2ad992318d0dbdb26e0eb8a3c7fa530e
Ganesh Sharma <ganeshsharmait@gmail.com>
2017-06-04 10:03:01 +0530

    Add app.py
    Python program to print a pyramid of asterisks on terminal.

---
+++ images/python/app.py
@@ -0,0 +1,11 @@
+def pyramid(rows):
+	print("Printing pyramid of stars!\n")
+	for i in range(1, rows+1):
+		for j in range(rows+1, i, -1):
+			print(" ",end="")			
+		for k in range (1, i+1):
+			print("* ",end="")
+		print("")
+	print("\n....... Done .......")
+
+pyramid(10)
